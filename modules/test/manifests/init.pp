f1df7aaa6b407e48409d0081cf1e570d5800aff4
vlytvyn <vlytvyn@Anamudi.softservecom.com>
2012-10-29 16:53:31 +0200

    dasd

--- modules/test/manifests/init.pp
+++ modules/test/manifests/init.pp
@@ -1,6 +1,6 @@
 class test () {
-  if $::test_variable == false {
+  if $test_variable == false {
     $test_variable = 'Hello World'
   }
-  notify {$::test_variable:}
+  notify {$test_variable:}
 }
