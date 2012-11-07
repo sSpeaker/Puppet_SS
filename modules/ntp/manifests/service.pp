aee82629604208796a1aa33750377441f3e835b9
vsinyay <vsinyay@Trango.softservecom.com>
2012-11-07 13:03:02 +0200

    1

--- modules/ntp/manifests/service.pp
+++ modules/ntp/manifests/service.pp
@@ -1,12 +1,13 @@
 class ntp::service (
-  $service_ensure = $ntp::params::service_ensure,
-  $service_name = $ntp::params::service_name,
-  $service_enable = $ntp::params::service_enable,
-  $service_hasstatus = $ntp::params::service_hasstatus,
-  $service_hasrestart = $ntp::params::service_hasrestart,
+  $service_ensure     = $ntp::params::service_ensure,
+  $service_name       = $ntp::params::service_name,
+  $service_enable     = $ntp::params::service_enable,
+  $service_hasstatus  = $ntp::params::service_hasstatus,
+  $service_hasrestart = $ntp::params::service_hasrestart
  ) inherits ntp::params {
+
   service { $ntp::service_name:
-    ensure     => $ntp::service_ensure_real,
+    ensure     => $ntp::service_ensure,
     enable     => $ntp::service_enable,
     hasstatus  => $ntp::service_hasstatus,
     hasrestart => $ntp::service_hasrestart,
