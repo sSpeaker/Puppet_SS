590738618c4ce6ce5e7593ec69de6598514510d4
vlytvyn <vlytvyn@Anamudi.softservecom.com>
2012-11-08 20:00:48 +0200

    change bdsm to hiera

--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -1,33 +1,33 @@
 class ntp (
-  $server_list = bdsm('g_ntp_server_list','ntp.ubuntu.com'),
-  $server_enabled = bdsm('g_ntp_server_enabled',true),
-  $query_networks = bdsm('g_ntp_query_networks',''),
-  $interface_ignore = bdsm('g_ntp_interface_ignore',''),
-  $interface_listen = bdsm('g_ntp_interface_listen',''),
-  $enable_statistics = bdsm('g_ntp_enable_statistics',true),
-  $statsdir = bdsm('g_ntp_statsdir',undef),
-  $config_file = bdsm('g_ntp_config_file','/etc/ntp.conf'),
-  $config_file_owner = bdsm('g_ntp_config_file_owner','root'),
-  $config_file_group = bdsm('g_ntp_config_file_group','root'),
-  $config_file_mode = bdsm('g_ntp_config_file_mode','0644'),
+  $server_list = hiera('g_ntp_server_list','ntp.ubuntu.com'),
+  $server_enabled = hiera('g_ntp_server_enabled',true),
+  $query_networks = hiera('g_ntp_query_networks',''),
+  $interface_ignore = hiera('g_ntp_interface_ignore',''),
+  $interface_listen = hiera('g_ntp_interface_listen',''),
+  $enable_statistics = hiera('g_ntp_enable_statistics',true),
+  $statsdir = hiera('g_ntp_statsdir',undef),
+  $config_file = hiera('g_ntp_config_file','/etc/ntp.conf'),
+  $config_file_owner = hiera('g_ntp_config_file_owner','root'),
+  $config_file_group = hiera('g_ntp_config_file_group','root'),
+  $config_file_mode = hiera('g_ntp_config_file_mode','0644'),
   $driftfile = undef
-  $ensure = bdsm('g_ntp_ensure','present'),
-  $autoupgrade = bdsm('g_ntp_autoupgrade',true),
-  $package = bdsm('g_ntp_package','ntp'),
-  $service_ensure = bdsm('g_ntp_service_ensure','running'),
+  $ensure = hiera('g_ntp_ensure','present'),
+  $autoupgrade = hiera('g_ntp_autoupgrade',true),
+  $package = hiera('g_ntp_package','ntp'),
+  $service_ensure = hiera('g_ntp_service_ensure','running'),
   $service_name = undef
-  $service_enable = bdsm('g_ntp_service_enable',true),
-  $use_install = bdsm('g_ntp_server_list',true),
-  $use_config = bdsm('g_ntp_server_list',true),
-  $use_service = bdsm('g_ntp_server_list',true),
+  $service_enable = hiera('g_ntp_service_enable',true),
+  $use_install = hiera('g_ntp_server_list',true),
+  $use_config = hiera('g_ntp_server_list',true),
+  $use_service = hiera('g_ntp_server_list',true),
 ) {
 
   if ! $service_name {
     $service_name = $::osfamily ? {
-		                    'Debian' => bdsm('g_ntp_service_name','ntp'),
-		                    'RedHat' => bdsm('g_ntp_service_name','ntpd'),
-		                    default  => bdsm('g_ntp_service_name',undef),
-		                }
+                        'Debian' => hiera('g_ntp_service_name','ntp'),
+                        'RedHat' => hiera('g_ntp_service_name','ntpd'),
+                        default  => hiera('g_ntp_service_name',undef),
+                    }
   }
 
   case $ensure {
@@ -50,15 +50,15 @@ class ntp (
 
       if $enable_statistics == true {
         if ! $driftfile {
-			    $driftfile = $::osfamily ? {
-			                   'Debian' => bdsm('g_ntp_driftfile','/var/lib/ntp/ntp.drift'),
-			                   'RedHat' => bdsm('g_ntp_driftfile','/var/lib/ntp/drift'),
-			                   default  => bdsm('g_ntp_driftfile',undef),
-			                 }
-				  if ! $statsdir {
+          $driftfile = $::osfamily ? {
+                         'Debian' => hiera('g_ntp_driftfile','/var/lib/ntp/ntp.drift'),
+                         'RedHat' => hiera('g_ntp_driftfile','/var/lib/ntp/drift'),
+                         default  => hiera('g_ntp_driftfile',undef),
+                       }
+          if ! $statsdir {
             fail('statsdir parameter must be set, if enable_statistics is true')
           }
-			  }
+        }
       }
     }
     /(absent)/: {
@@ -70,10 +70,10 @@ class ntp (
     }
   }
 
-	include ntp::install
-	include ntp::config
-	include ntp::service
-	
+  include ntp::install
+  include ntp::config
+  include ntp::service
+  
   Class['ntp::install']-> Class['ntp::service'] -> Class['ntp::config']
 
 }
