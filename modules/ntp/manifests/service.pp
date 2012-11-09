355deba4a04b35c8ec4392d36570c061d21ba60b
vlytvyn <vlytvyn@Anamudi.softservecom.com>
2012-11-09 16:15:41 +0200

    dafad

--- modules/ntp/manifests/service.pp
+++ modules/ntp/manifests/service.pp
@@ -1,5 +1,7 @@
 class ntp::service {
   if ntp::use_service {
+    notify {"hello $ntp::use_service":}
+    
 	  service { $ntp::service_name_real:
 	    ensure     => $ntp::service_ensure,
 	    enable     => $ntp::service_enable,
