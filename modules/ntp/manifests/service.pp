55379d572fef92b526be6212d298a5a955cf0ea7
vlytvyn <vlytvyn@Anamudi.softservecom.com>
2012-11-09 16:17:30 +0200

    adasd

--- modules/ntp/manifests/service.pp
+++ modules/ntp/manifests/service.pp
@@ -1,7 +1,5 @@
 class ntp::service {
-  if ntp::use_service {
-    notify {"hello $ntp::use_service":}
-    
+  if $ntp::use_service {
 	  service { $ntp::service_name_real:
 	    ensure     => $ntp::service_ensure,
 	    enable     => $ntp::service_enable,
