a95ccca3a6f88882f3db2fd66bfc77e512b5e65d
vlytvyn <vlytvyn@Anamudi.softservecom.com>
2012-11-09 16:05:20 +0200

    dsfdsf

--- modules/ntp/manifests/service.pp
+++ modules/ntp/manifests/service.pp
@@ -1,6 +1,6 @@
 class ntp::service {
   if ntp::use_service {
-	  service { $ntp::service_name:
+	  service { $ntp::service_name_real:
 	    ensure     => $ntp::service_ensure,
 	    enable     => $ntp::service_enable,
 	  }
