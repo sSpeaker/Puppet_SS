0eda709748d89d8076d54630ae084c252f596244
vlytvyn <vlytvyn@Anamudi.softservecom.com>
2012-11-16 17:35:47 +0200

    222

--- modules/module_name/manifests/config.pp
+++ modules/module_name/manifests/config.pp
@@ -1,7 +1,9 @@
+#
+#
 class module_name::config {
   # If module_name_manage_config = false then do nothing.
   # For better resolving dependencies cycles.
-    if str2bool("$module_name::module_name_manage_config") {
+  if str2bool("$module_name::module_name_manage_config") {
 	  file { $module_name::module_name_config_file:
 			ensure  => $module_name::module_name_ensure,
 			owner   => $module_name::module_name_config_file_owner,
--- modules/module_name/manifests/init.pp
+++ modules/module_name/manifests/init.pp
@@ -1,15 +1,15 @@
+# We can set this parameters from declaration of class, ENC or in hiera.
+# If this class was not included via declaration resource 'class' or ENC didn`t set class variables
+# Puppet will check every parameters via hiera.
+#
 class module_name (
-  # Array of module variables.
-  # We can set this variables from declaration of class, ENC or in hiera.
-  # If this class was not included via declaration resource 'class' or ENC didn`t set class variables
-  # Puppet will check every variable via hiera.
   $module_name_service_name = undef,
   $module_name_service_enable = undef,
   $module_name_manage_install = true,
   $module_name_manage_config = true,
   $module_name_manage_service = true,
 ) {
-  # Logic of setting internal module variables
+  # Set service name depend on Operation System
   if ! $module_name_service_name {
     $module_name_service_name_real = $::osfamily ? {
                         'Debian' => hiera('module_name_service_name','ntp'),
@@ -20,7 +20,9 @@ class module_name (
    $module_name_service_name_real = $module_name_service_name
   }
   
-  # Dependency block. Always should be at the end of first class.
+  # Define class dependency order to meet the rule: install, configure, start/stop
   Class['module_name::install']-> Class['module_name::config'] -> Class['module_name::service']
+
+  # Install, Configure and Manage service 
   include module_name::install, module_name::config, module_name::service
 }
