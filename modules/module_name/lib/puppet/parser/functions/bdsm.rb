8af74b32e0c3ac7cf575c330f56b106ebed0ac48
vlytvyn <vlytvyn@Anamudi.softservecom.com>
2012-11-16 12:51:59 +0200

    1

--- modules/module_name/lib/puppet/parser/functions/bdsm.rb
+++
@@ -1,6 +0,0 @@
-module Puppet::Parser::Functions
-  newfunction(:bdsm, :type => :rvalue) do |args|
-    !lookupvar(args[0]).nil? ? output=lookupvar(args[0]) : output=args[1]
-    return output
-  end
-end
