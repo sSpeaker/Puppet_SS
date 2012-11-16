4bd516c1b79a836a338874310e8dcb7d7a5340a9
vlytvyn <vlytvyn@Anamudi.softservecom.com>
2012-11-16 17:53:47 +0200

    122

--- modules/module_name/manifests/config.pp
+++ modules/module_name/manifests/config.pp
@@ -6,7 +6,7 @@ class module_name::config {
   if str2bool("$module_name::module_name_manage_config") {
 	  file { $module_name::module_name_config_file:
 			ensure  => $module_name::module_name_ensure,
-			owner   => $module_name::module_name_config_file_owner,
+		          	owner   => $module_name::module_name_config_file_owner,
 			group   => $module_name::module_name_config_file_group,
 			mode    => $module_name::module_name_config_file_mode,
 			content => template('module_name/module_name.conf.erb'),
