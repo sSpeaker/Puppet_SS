4c03764323b5500e389f5197d96dc8d285a457c3
vlytvyn <vlytvyn@Anamudi.softservecom.com>
2012-11-19 17:01:37 +0200

    dasasd

--- scripts/enc.sh
+++ scripts/enc.sh
@@ -2,8 +2,8 @@
 
 FQDN=$1
 
-[ -f "/etc/puppet/$ENVIRONMENT/enc/${FQDN}.yaml" ] && {
-   ANSWER=`cat "/etc/puppet/$ENVIRONMENT/enc/${FQDN}.yaml"`
+[ -f "/etc/puppet/$PUPPET_ENVIRONMENT/enc/${FQDN}.yaml" ] && {
+   ANSWER=`cat "/etc/puppet/$PUPPET_ENVIRONMENT/enc/${FQDN}.yaml"`
 } || {
    ANSWER="`/usr/bin/env PUPPET_DASHBOARD_URL=http://localhost:3000 /usr/share/puppet-dashboard/bin/external_node $FQDN`"
 }
