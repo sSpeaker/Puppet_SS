5f62f0b82080b995b9a710b43a0cedd90b1007d1
vlytvyn <vlytvyn@Anamudi.softservecom.com>
2012-11-19 17:04:38 +0200

    dasdasd

--- scripts/enc.sh
+++ scripts/enc.sh
@@ -3,7 +3,8 @@
 FQDN=$1
 
 [ -f "/etc/puppet/$PUPPET_ENVIRONMENT/enc/${FQDN}.yaml" ] && {
-   ANSWER=`cat "/etc/puppet/$PUPPET_ENVIRONMENT/enc/${FQDN}.yaml"`
+   ENC_LOCALFILE="/etc/puppet/$PUPPET_ENVIRONMENT/enc/${FQDN}.yaml"
+   ANSWER=`cat $ENC_LOCALFILE | sed "s/parameters:/parameters:\n  enc_localfile: $ENC_LOCALFILE/"`
 } || {
    ANSWER="`/usr/bin/env PUPPET_DASHBOARD_URL=http://localhost:3000 /usr/share/puppet-dashboard/bin/external_node $FQDN`"
 }
