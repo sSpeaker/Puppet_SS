1f2a3a3b78c9ee8646dcc3d9c12e2333e78ac021
vlytvyn <vlytvyn@Anamudi.softservecom.com>
2012-11-19 16:51:44 +0200

    111

---
+++ manifests/site.pp
@@ -0,0 +1,20 @@
+if $enc_localfile {
+  notify {"Using node config from: $enc_localfile":
+    stage => 'pre', 
+  }
+} else {
+  notify {"Using node config from: Dashboard"}
+}
+
+stage { 'pre':
+  before => Stage['main'],
+}
+
+notify {"Message: $test_var":}
+
+filebucket { "main":
+  server => "puppet",
+  path => false,
+}
+
+File { backup => "main" }
